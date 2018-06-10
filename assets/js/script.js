a70f852da3d464135da22dcc2843bf40ad53c737
Heather Olcot <hfolcot@gmail.com>
2018-06-10 13:41:22 +0000

    completed basic js functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
-var searchTerms
+var searchTerms;
+var trackID;
 
 function checkRadio() {
     searchTerms = document.getElementById("search").value;
@@ -12,13 +13,17 @@ function checkRadio() {
 }
 
 function getTrack() {
+    var resultsSection = document.getElementById("results");
+    resultsSection.innerHTML = "";
     $.ajax({
             type: "GET",
             data: {
                 apikey: "16099f064260947071709a4bc6421891",
-                q_artist: searchTerms,
+                q_track: searchTerms,
                 format: "jsonp",
                 callback: "jsonp_callback",
+                page_size: 50,
+                s_artist_rating: "DESC",
 
             },
 
@@ -27,8 +32,13 @@ function getTrack() {
             jsonpCallback: 'jsonp_callback',
             contentType: 'application/json',
             success: function(data) {
-                console.log(data)
-
+                console.log(data);
+                var trackResults = data.message.body.track_list;
+                console.dir(trackResults);
+                trackResults.forEach(function(item) {
+                    console.log(trackID);
+                    resultsSection.innerHTML += `<p>${item.track.track_name} by ${item.track.artist_name}<button onclick="getLyrics(${item.track.track_id})">Click here for lyrics</button></p>`;
+                });
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 console.log(jqXHR);
@@ -37,12 +47,13 @@ function getTrack() {
             }
         }
 
-    )
+    );
 }
 
 function getArtist() {
     var resultsSection = document.getElementById("results");
     resultsSection.innerHTML = "";
+    trackID = 
     $.ajax({
             type: "GET",
             data: {
@@ -50,7 +61,8 @@ function getArtist() {
                 q_artist: searchTerms,
                 format: "jsonp",
                 callback: "jsonp_callback",
-                page_size: 50
+                page_size: 50,
+                s_artist_rating: "DESC",
 
             },
 
@@ -63,9 +75,9 @@ function getArtist() {
                 var artistResults = data.message.body.artist_list;
                 console.dir(artistResults);
                 artistResults.forEach(function(item) {
-                    resultsSection.innerHTML += `<p>${item.artist.artist_name}</p>`;
+                    resultsSection.innerHTML += `<p>${item.artist.artist_name}<button onclick="getAlbumList(${item.artist.artist_id})">Click here for a list of albums</button></p>`;
                 });
-                
+
             },
             error: function(jqXHR, textStatus, errorThrown) {
                 console.log(jqXHR);
@@ -74,28 +86,101 @@ function getArtist() {
             }
         }
 
-    )
+    );
 }
 
-/*function getLyrics() {
+function getLyrics(trackID) {
+    var resultsSection = document.getElementById("results");
+    resultsSection.innerHTML = "";
     $.ajax({
             type: "GET",
             data: {
                 apikey: "16099f064260947071709a4bc6421891",
-                q_track: searchTerms,
-                q_artist: searchTerms,
+                track_id: trackID,
                 format: "jsonp",
-                callback: "callback",
+                callback: "jsonp_callback",
+                page_size: 50,
 
             },
-            contentType: 'application/json',
             url: "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get",
-            jsonpCallback: 'callback',
-            success: function() {
-                console.log(data)
+            dataType: "jsonp",
+            jsonpCallback: 'jsonp_callback',
+            contentType: 'application/json',
+            success: function(data) {
+                console.log(data);
+                var lyricResults = data.message.body.lyrics.lyrics_body;
+                console.dir(lyricResults);
+                resultsSection.innerHTML += `<p>${lyricResults}</p>`;
+
+
+
             }
         }
 
-    )
+    );
 }
-*/
+function getAlbumList(artistID) {
+    var resultsSection = document.getElementById("results");
+    resultsSection.innerHTML = "";
+    $.ajax({
+            type: "GET",
+            data: {
+                apikey: "16099f064260947071709a4bc6421891",
+                artist_id: artistID,
+                format: "jsonp",
+                callback: "jsonp_callback",
+                page_size: 50,
+
+            },
+            url: "https://api.musixmatch.com/ws/1.1/artist.albums.get",
+            dataType: "jsonp",
+            jsonpCallback: 'jsonp_callback',
+            contentType: 'application/json',
+            success: function(data) {
+                console.log(data);
+                var albumList = data.message.body.album_list;
+                console.dir(albumList);
+                albumList.forEach(function(item) {
+                    resultsSection.innerHTML += `<p>${item.album.album_name}<button onclick="getTrackList(${item.album.album_id})">Click here for a list of tracks</button></p>`;
+                });
+                
+    
+
+
+            }
+        }
+
+    );
+}
+
+function getTrackList(albumID) {
+    var resultsSection = document.getElementById("results");
+    resultsSection.innerHTML = "";
+    $.ajax({
+            type: "GET",
+            data: {
+                apikey: "16099f064260947071709a4bc6421891",
+                album_id: albumID,
+                format: "jsonp",
+                callback: "jsonp_callback",
+                page_size: 50,
+
+            },
+            url: "https://api.musixmatch.com/ws/1.1/album.tracks.get",
+            dataType: "jsonp",
+            jsonpCallback: 'jsonp_callback',
+            contentType: 'application/json',
+            success: function(data) {
+                console.log(data);
+                var trackResults = data.message.body.track_list;
+                console.dir(trackResults);
+                trackResults.forEach(function(item) {
+                    resultsSection.innerHTML += `<p>${item.track.track_name}<button onclick="getLyrics(${item.track.track_id})">Click here for lyrics</button></p>`;
+                });
+
+
+            }
+        }
+
+    );
+}
