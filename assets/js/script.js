960877a1e714246bed4719304f58efd3d22c963c
Heather Olcot <hfolcot@gmail.com>
2018-06-15 10:48:57 +0000

    changed output results to form a table instead of divs

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,7 +29,7 @@ function getTrack() {
                 q_track: searchTerms,
                 format: "jsonp",
                 callback: "jsonp_callback",
-                page_size: 50,
+                page_size: 100,
                 s_artist_rating: "DESC",
 
             },
@@ -42,21 +42,28 @@ function getTrack() {
                 console.log(data);
                 var trackResults = data.message.body.track_list;
                 console.dir(trackResults);
+                resultsSection.innerHTML += `<thead>
+                                                <tr>
+                                                  <th scope="col">Track Name</th>
+                                                  <th scope="col">Artist</th>
+                                                  <th scope="col">Lyrics</th>
+                                                </tr>
+                                             </thead>`;
                 trackResults.forEach(function(item) {
-                    console.log(trackID);
-                    resultsSection.innerHTML += `<div class="row justify-content-center">
-                                                    <div class="col-4 result-container">
-                                                        <p>${item.track.track_name}</p>
-                                                    </div>
-                                                    <div class="col-4 result-container">
-                                                        <p>${item.track.artist_name}</p>
-                                                    </div>
-                                                    <div class="col-3 result-container">
-                                                        <button class="btn btn-primary btn-result" onclick="getLyrics(${item.track.track_id})">Click here for lyrics</button>
-                                                        <button class="btn btn-primary btn-result-mobile" onclick="getLyrics(${item.track.track_id})">Lyrics</button>
-                                                    </div>
-                                                </div>`;
+                    resultsSection.innerHTML += `<tbody>
+                                                    <tr>
+                                                        <td>${item.track.track_name}</td>
+                                                        <td>${item.track.artist_name}</td>
+                                                        <td>
+                                                            <button class="btn btn-secondary btn-result" onclick="getLyrics(${item.track.track_id}, '${item.track.track_name}')">Click here for lyrics</button>
+                                                            <button class="btn btn-secondary btn-result-mobile" onclick="getLyrics(${item.track.track_id}, ${item.track.track_name})">Lyrics</button>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>`;
+
                 });
+
+
             },
 
         }
@@ -67,56 +74,75 @@ function getTrack() {
 //getArtist is run when the user has searched for an artist.
 function getArtist() {
     resetPage();
-        $.ajax({
-                type: "GET",
-                data: {
-                    apikey: "16099f064260947071709a4bc6421891",
-                    q_artist: searchTerms,
-                    format: "jsonp",
-                    callback: "jsonp_callback",
-                    page_size: 50,
-                    s_artist_rating: "DESC",
-
-                },
-
-                url: "https://api.musixmatch.com/ws/1.1/artist.search",
-                dataType: "jsonp",
-                jsonpCallback: 'jsonp_callback',
-                contentType: 'application/json',
-                success: function(data) {
-                    console.log(data);
-                    var artistResults = data.message.body.artist_list;
-                    console.dir(artistResults);
-                    artistResults.forEach(function(item) {
-                        resultsSection.innerHTML += `<div class="row justify-content-center">
-                                                        <div class="col-4 result-container">
-                                                            <p>${item.artist.artist_name}</p>
-                                                        </div>
-                                                        <div class="col-4 result-container">
-                                                            <button class="btn btn-primary btn-result" onclick="getAlbumList(${item.artist.artist_id})">Click here for a list of albums</button>
-                                                            <button class="btn btn-primary btn-result-mobile" onclick="getAlbumList(${item.artist.artist_id})">Albums</button>
-                                                        </div>
-                                                    </div>`;
-                    });
-
-                },
-
-            }
-
-        );
-}
-
-//getLyrics is run when the user has selected the track they want lyrics for. 
-function getLyrics(trackID) {
-    resetPage();
     $.ajax({
             type: "GET",
             data: {
                 apikey: "16099f064260947071709a4bc6421891",
-                track_id: trackID,
+                q_artist: searchTerms,
                 format: "jsonp",
                 callback: "jsonp_callback",
                 page_size: 50,
+                s_artist_rating: "DESC",
+
+            },
+
+            url: "https://api.musixmatch.com/ws/1.1/artist.search",
+            dataType: "jsonp",
+            jsonpCallback: 'jsonp_callback',
+            contentType: 'application/json',
+            success: function(data) {
+                console.log(data);
+                var artistResults = data.message.body.artist_list;
+                console.dir(artistResults);
+                resultsSection.innerHTML += `<thead>
+                                                <tr>
+                                                  <th scope="col">Artist Name</th>
+                                                  <th scope="col">Albums</th>
+                                                </tr>
+                                             </thead>`;
+                artistResults.forEach(function(item) {
+                    resultsSection.innerHTML += `<tbody>
+                                                        <tr>
+                                                        <td>${item.artist.artist_name}</td>
+                                                        <td>
+                                                            <button class="btn btn-secondary btn-result" onclick="getAlbumList(${item.artist.artist_id})">Click here for a list of albums</button>
+                                                                <button class="btn btn-secondary btn-result-mobile" onclick="getAlbumList(${item.artist.artist_id})">Albums</button>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>`;
+
+                });
+                if (artistResults.length === 0) {
+                    resetPage();
+                    resultsSection.innerHTML += `<thead>
+                                                    <tr>
+                                                        <th scope="col">A problem has occurred</th>
+                                                    </tr>
+                                                </thead>
+                                                <tbody>
+                                                    <tr>
+                                                        <td>Sorry, no results were found.</td>
+                                                    </tr>
+                                                </tbody>`;
+                }
+
+            },
+
+        }
+
+    );
+}
+
+//getLyrics is run when the user has selected the track they want lyrics for. 
+function getLyrics(trackID, trackName) {
+    resetPage();
+    $.ajax({
+            type: "GET",
+            data: {
+                apikey: "16099f064260947071709a4bc6421891", 
+                track_id: trackID,
+                format: "jsonp",
+                callback: "jsonp_callback",
 
             },
             url: "https://api.musixmatch.com/ws/1.1/matcher.lyrics.get",
@@ -130,21 +156,29 @@ function getLyrics(trackID) {
                     var lyricCopyright = data.message.body.lyrics.lyrics_copyright;
                 }
                 catch (err) {
-                    resultsSection.innerHTML += `<div class="row justify-content-center">
-                                                <div class="col-10 lyric-container">
-                                                <p>Sorry, there are no lyrics available for this song.</p>
-                                                </div>
-                                             </div>`;
-                                             return;
-                    
+                    resultsSection.innerHTML += `<thead>
+                                                <tr>
+                                                  <th scope="col">A problem has occurred</th>
+                                                </tr>
+                                             </thead>
+                                             <tbody>
+                                                <tr>
+                                                    <td>Sorry, there are no lyrics available for this song.</td>
+                                                </tr>
+                                             </tbody>`;
+                    return;
+
                 }
-                resultsSection.innerHTML += `<div class="row justify-content-center">
-                                                <div class="col-10 lyric-container">
-                                                ${lyricResults}<br>${lyricCopyright}
-                                                </div>
-                                             </div>`;
-
-
+                resultsSection.innerHTML += `<thead>
+                                                <tr>
+                                                  <th scope="col">${trackName}</th>
+                                                </tr>
+                                             </thead>
+                                             <tbody>
+                                                <tr>
+                                                    <td>${lyricResults}<br>${lyricCopyright}</td>
+                                                </tr>
+                                             </tbody>`;
 
             }
         }
@@ -162,7 +196,8 @@ function getAlbumList(artistID) {
                 artist_id: artistID,
                 format: "jsonp",
                 callback: "jsonp_callback",
-                page_size: 50,
+                page_size: 100,
+                g_album_name: 1
 
             },
             url: "https://api.musixmatch.com/ws/1.1/artist.albums.get",
@@ -173,17 +208,24 @@ function getAlbumList(artistID) {
                 console.log(data);
                 var albumList = data.message.body.album_list;
                 console.dir(albumList);
+                resultsSection.innerHTML += `<thead>
+                                                <tr>
+                                                  <th scope="col">Album Name</th>
+                                                  <th scope="col">Track List</th>
+                                                </tr>
+                                             </thead>`;
                 albumList.forEach(function(item) {
-                    resultsSection.innerHTML += `<div class="row justify-content-center">
-                                                    <div class="col-4 result-container">
-                                                        <p>${item.album.album_name}</p>
-                                                    </div>
-                                                    <div class="col-4 result-container">
-                                                        <button class="btn btn-primary btn-result" onclick="getTrackList(${item.album.album_id})">Click here for a list of tracks</button>
-                                                        <button class="btn btn-primary btn-result-mobile" onclick="getTrackList(${item.album.album_id})">Tracks</button>
-                                                    </div>
-                                                </div>`;
+                    resultsSection.innerHTML += `<tbody>
+                                                    <tr>
+                                                        <td>${item.album.album_name}</td>
+                                                        <td>
+                                                            <button class="btn btn-secondary btn-result" onclick="getTrackList(${item.album.album_id})">Click here for a list of tracks</button>
+                                                            <button class="btn btn-secondary btn-result-mobile" onclick="getTrackList(${item.album.album_id})">Tracks</button>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>`;
                 });
+
 
 
 
@@ -215,16 +257,22 @@ function getTrackList(albumID) {
                 console.log(data);
                 var trackResults = data.message.body.track_list;
                 console.dir(trackResults);
+                resultsSection.innerHTML += `<thead>
+                                                <tr>
+                                                  <th scope="col">Track Name</th>
+                                                  <th scope="col">Lyrics</th>
+                                                </tr>
+                                             </thead>`;
                 trackResults.forEach(function(item) {
-                    resultsSection.innerHTML += `<div class="row justify-content-center">
-                                                    <div class="col-4 result-container">
-                                                        <p>${item.track.track_name}</p>
-                                                    </div>
-                                                    <div class="col-4 result-container">
-                                                        <button class="btn btn-primary btn-result" onclick="getLyrics(${item.track.track_id})">Click here for lyrics</button>
-                                                        <button class="btn btn-primary btn-result-mobile" onclick="getLyrics(${item.track.track_id})">Lyrics</button>
-                                                    </div>
-                                                </div>`;
+                    resultsSection.innerHTML += `<tbody>
+                                                    <tr>
+                                                        <td>${item.track.track_name}</td>
+                                                        <td>
+                                                            <button class="btn btn-secondary btn-result" onclick="getLyrics(${item.track.track_id}, '${item.track.track_name}')">Click here for lyrics</button>
+                                                            <button class="btn btn-secondary btn-result-mobile" onclick="getLyrics(${item.track.track_id}, '${item.track.track_name}')">Lyrics</button>
+                                                        </td>
+                                                    </tr>
+                                                </tbody>`;
                 });
 
 
