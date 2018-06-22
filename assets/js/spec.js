707a19566f6255986b27933ef5b8775fddfe40ad
Heather Olcot <hfolcot@gmail.com>
2018-06-22 08:21:28 +0000

    removed commented out code from index.html and added to spec.js to make more sense

--- assets/js/spec.js
+++ assets/js/spec.js
@@ -10,3 +10,12 @@ describe("Lyric Finder", function() {
             });
         });
 });
+
+
+    
+/*Add to index.html to use Jasmine testing
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jasmine/3.1.0/jasmine.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jasmine/3.1.0/jasmine-html.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jasmine/3.1.0/boot.js"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jasmine/3.1.0/jasmine.css" />
+<script type="text/javascript" src="assets/js/spec.js"></script>*/
